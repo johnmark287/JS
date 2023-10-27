adaae39d5a41cee3455957814b577375e63e76cb
johnmark287 <johnmark.muhando@students.jkuat.ac.ke>
2023-10-27 23:10:30 +0300

    Demo

---
+++ test.js
@@ -0,0 +1,14 @@
+function myFunction()
+{
+	document.getElementById("demo").innerHTML = "Mark.";
+}
+
+function myFunction1()
+{
+	document.getElementById("demo1").innerHTML = "Benjamin."
+}
+
+var x;
+let y, z;
+
+z = x + y;
