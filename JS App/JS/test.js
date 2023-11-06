4d73148f539a673cb8316d2e979bba07d76a3091
johnmark287 <johnmark.muhando@students.jkuat.ac.ke>
2023-11-06 19:48:22 +0300

    update

---
+++ JS App/JS/test.js
@@ -0,0 +1,24 @@
+{
+    detail:[
+        {
+            "path": ["username"],
+            "message":"username taken"
+        },
+        {
+            "path": ["username"],
+            "message":"invalid password"
+        }
+    ]
+}
+
+{
+    username: "username taken",
+    password: "week pasword"
+}
+
+// Assignment
+
+parseJSONErrror(error)
+{
+    return // 1 statement
+}
