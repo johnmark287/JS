b15a22e15049a7216087a06ec423b7d12cc2f744
johnmark287 <johnmark.muhando@students.jkuat.ac.ke>
2023-11-10 19:59:00 +0300

    add , deleting and modifying values of the html file.

--- JS App/JS/DOM/styling.js
+++ JS App/JS/DOM/styling.js
@@ -1,5 +1,28 @@
-//DOM Manipulation
+// //DOM Manipulation
 
-const title = document.querySelector('#main-heading');
-title.style.color = 'red';
-console.log(title);
+// const title = document.querySelector('#main-heading');
+// title.style.color = 'red';
+// console.log(title);
+
+// // adding elements
+// const ul = document.querySelector('ul');
+const li = document.createElement('li');
+
+ul.append(li);
+
+// modifying the text
+li.innerText = 'Transporter';
+
+// Modifying attributes & classes
+li.setAttribute('id', 'main-heading');
+li.removeAttribute('id');
+
+const title = document.querySelector('main-heading');
+
+console.log(title.getAttribute('id'));
+
+li.classList.add('list-item');
+li.classList.remove('list-item');
+
+// Remove elements
+li.remove();
